0fb02ac677ebee43795f3b6cac295adda7c8ce94
PantsuDango <394883561@qq.com>
2021-05-16 14:38:12 +0800

    项目初始化

--- git.sh
+++ git.sh
@@ -4,15 +4,15 @@ git pull
 path=$PWD
 
 cd $path/server
-num=$(netstat -nlpt | grep ":::6666" | grep -v "grep" | wc -l)
+num=$(netstat -nlpt | grep "0.0.0.0:6666" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    kill -9 $(ps -ef | grep "person_manage" | grep -v "grep" | awk '{print $2}')
+    kill -9 $(netstat -nlpt | grep "0.0.0.0:6666" | awk '{print $7}' | awk -F '/' '{print $1}')
     sleep 3
 fi
 nohup python3 person_manage.py &
 sleep 3
 
-num=$(netstat -nlpt | grep ":::6666" | grep -v "grep" | wc -l)
+num=$(netstat -nlpt | grep "0.0.0.0:6666" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
     echo "run person_manage success"
 else
